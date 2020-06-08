7fcbf80a9af92475b1518216a58fb63b5fc3867c
nzachari <nzachari@eecs.wsu.edu>
2020-06-08 13:05:46 -0700

    have title

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from './Title.js'
+// import { BrowserRouter, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div className = "App">
+            <Title />
+        </div>
+    )
+}
+
+export default App;
---
+++ client/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css'
+
+const Title = () => {
+    return (
+        <div className="Title"> 
+            <h1>Wallpaper</h1>
+        </div>
+    );
+}
+
+export default Title;
